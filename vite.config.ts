8c867fb0759ceca8b94d18b5f569afbecac9e2e2
RogKenzas <hallokensas@gmail.com>
2026-08-21 05:36:05 +0200

    Resolution du bug de CORS

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://backend-planify-jsk6.onrender.com",
+      "/api": "http://localhost:10000",
     },
   },
 })
